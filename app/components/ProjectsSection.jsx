a14139cf97f51793b4348e3d533910d99f650b86
Blistt <farriagapazos@miners.utep.edu>
2024-05-17 02:16:05 -0600

    about and home merged

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 
+const ProjectsData =[
+  {
+    id: 1,
+    title: 'Project 1',
+    description: 'This is a description of project 1',
+    image: '/images/project1.png',
+    link: 'https://www.google.com',
+  },
+  {
+    id: 2,
+    title: 'Project 2',
+    description: 'This is a description of project 2',
+    image: '/images/project2.png',
+    link: 'https://www.google.com'
+  },
+  {
+    id: 3,
+    title: 'Project 3',
+    description: 'This is a description of project 3',
+    image: '/images/project3.png',
+    link: 'https://www.google.com'
+  }
+]
+
 const ProjectsSection = () => {
   return (
     <h2>
--- app/page.js
+++ app/page.js
@@ -1,15 +1,13 @@
 import Image from "next/image";
-import HeroSection from "./components/HeroSection";
+import HomeSection from "./components/HomeSection";
 import Navbar from "./components/Navbar";
-import AboutSection from "./components/AboutSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div class="container mt-24 mx-auto py-4 px-12">
-        <HeroSection />
-        <AboutSection />
+        <HomeSection />
       </div>
     </main>
   );
